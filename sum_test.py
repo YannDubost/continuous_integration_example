8ad6d1565aa1dec79e55a5f0910a7803bb8d11be
dubos <yann.dubost@etu.univ-smb.fr>
2021-01-13 09:25:22 +0100

    dghjk

--- sum_test.py
+++ sum_test.py
@@ -6,10 +6,12 @@ from sum import *
     ([1, 2, 3, 4, 5], 15),
     ([3, 2, 5], 10),
     ([1, 2, 3, 4, 5, 8, 1], 24),
-    ([1, 2, 3, 4, 5, 9, 3], 27)
+    ([1, 2, 3, 4, 5, 9, "Thomas"], 27)
     # Ajoutez des tests pour que la somme des éléments de la liste soit égale à 10, 24 et 27
 ])
 def test_mysum(numbers, output):
     assert mysum(numbers) == output
     assert type(numbers[0]) == int
     assert (len(numbers) % 2) == 1
+    for i in numbers:
+        assert type(i) == int
